8529f0c711a39daf802bf05140b9c6b43fa3d5fd
solfe_vue <solfe_vue@da9893.com>
2025-09-02 17:42:50 +0900

    title修改

--- src/pages/home/index.config.ts
+++ src/pages/home/index.config.ts
@@ -1,5 +1,5 @@
 export default definePageConfig({
-  navigationBarTitleText: "首页",
+  navigationBarTitleText: "日语学习工具",
   enableShareAppMessage: true, // 允许分享给朋友
   enableShareTimeline: true, // 允许分享到朋友圈
 });
