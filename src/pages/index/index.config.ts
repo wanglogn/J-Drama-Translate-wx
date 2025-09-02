3e2d964f9714fd13f522663db8d37733e6897ee1
solfe_vue <solfe_vue@da9893.com>
2025-09-02 17:45:32 +0900

    title修改

--- src/pages/index/index.config.ts
+++ src/pages/index/index.config.ts
@@ -1,5 +1,5 @@
 export default definePageConfig({
-  navigationBarTitleText: "台词翻译",
+  navigationBarTitleText: "日语会话精选",
   enableShareAppMessage: true, // 允许分享给朋友
   enableShareTimeline: true, // 允许分享到朋友圈
 });
--- src/pages/jpVerb/index.config.ts
+++ src/pages/jpVerb/index.config.ts
@@ -1,5 +1,5 @@
 export default definePageConfig({
-  navigationBarTitleText: "动词变形",
+  navigationBarTitleText: "日语动词变形",
   enableShareAppMessage: true, // 允许分享给朋友
   enableShareTimeline: true, // 允许分享到朋友圈
 });
